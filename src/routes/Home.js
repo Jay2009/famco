4433f4fa7b37db1f570ca227a7dd5a6c2b5bc8cf
Jay2009 <37827213+Jay2009@users.noreply.github.com>
2021-09-30 01:46:56 +0900

    done with creat on database

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,15 +1,40 @@
 import { dbService } from "fbase";
-import { addDoc, collection } from "firebase/firestore";
-import React, { useState } from "react";
+import { addDoc, collection, getDocs, query, onSnapshot, orderBy } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
     const [NewFamcoMsg, setNewFamcoMsg] = useState("");
+    const [NewFamcoMsges, setNewFamcoMsges] = useState([]);
+    const getNewFamcoMsges = async() => {
+        const DbNewFamcoMsges = query(collection(dbService,"NewFamcoMsges"));
+        const querySnapshot = await getDocs(DbNewFamcoMsges);
+        querySnapshot.forEach((doc) => {
+            const newFamcoMsgObj = {
+                ...doc.date(),
+                id: doc.id,
+            }
+            setNewFamcoMsges(prev => [newFamcoMsgObj, ...prev]);
+        });
+    };
+    useEffect (() => {
+        const q = query(
+            collection(dbService, "nweets"),
+            orderBy("createdAt", "desc")
+            );
+            onSnapshot(q, (snapshot) => {
+            const nweetArr = snapshot.docs.map((doc) => ({
+            id: doc.id,
+            ...doc.data(),
+            }));
+            setNewFamcoMsges(nweetArr);
+            });
+            }, []);
     const onSubmit = async(event) => {       
         event.preventDefault();
         try {
             const docRef = await addDoc(collection(dbService, "NewFamcoMsg"), {
                 NewFamcoMsg,
-                msgtime: Date.now(),
+                createdAt: Date.now(),
             });
             console.log("Document written with ID: ", docRef.id);
             } catch (error) {
@@ -38,7 +63,14 @@ const Home = () => {
             value="Post" 
             />
         </form>
+        <div>
+            {NewFamcoMsges.map((NewFamcoMsg) => (
+                <div key={NewFamcoMsg.id}>
+                    <h4>{NewFamcoMsg.NewFamcoMsg}</h4>
+                </div>
+            ))}
+        </div>
     </div>
     );
-}
+};
 export default Home;
